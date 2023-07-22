ce4097668997c037fe61f12f02b339d12d7adac6
Yash Garg <60708843+yash0501@users.noreply.github.com>
2023-07-22 23:56:30 +0530

    Create Lottery.sol

---
+++ contracts/Lottery.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract Lottery {
+    address owner;
+    uint256 maxTickets;
+    uint256 ticketsAvailable;
+    address payable[] ticketBuyers;
+    uint256 ticketPrice;
+
+    constructor(uint256 _maxTickets, uint256 _price){
+        owner = msg.sender;
+        maxTickets = _maxTickets;
+        ticketPrice = _price;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Not Owner");
+        _;
+    }
+
+    function buyTicket() public payable {
+        require(ticketsAvailable > 0, "No Tickets Available");
+        require(msg.value >= ticketPrice, "Invalid amount sent");
+        ticketBuyers.push(payable(msg.sender));
+    }
+
+    function endGame() public payable {
+        require(ticketsAvailable == 0, "Game is yet to end");
+        uint256 winnerId = block.timestamp % maxTickets;
+        address payable winnerAddress = ticketBuyers[winnerId];
+        (bool sent, bytes memory data) = winnerAddress.call{value: address(this).balance}("");
+        require(sent, "Failed to send Ether");
+        ticketBuyers = new address payable[](0);
+        ticketsAvailable = maxTickets;
+    }
+
+    function checkGameStatus() public view returns (string memory) {
+        if(ticketsAvailable > 0) {
+            return "Game still on";
+        }
+        else {
+            return "Game over!";
+        }
+    }
+}
